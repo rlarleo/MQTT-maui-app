bc185ac358f8dec084d9af678c761b7d9249c8bd
Kidae-Kim <kidae.kim@daydreamlab.co.kr>
2024-02-28 09:38:00 +0900

    :tada: init

---
+++ MauiApp2/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.LocalNotification;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		LocalNotificationCenter.Current.NotificationActionTapped += Current_NotificationActionTapped;
+	}
+
+	private void Current_NotificationActionTapped(Plugin.LocalNotification.EventArgs.NotificationActionEventArgs e)
+	{
+		if (e.IsDismissed)
+		{
+
+		}
+		else if (e.IsTapped)
+		{
+
+		}
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+		
+		var request = new NotificationRequest
+		{
+			NotificationId = 1337,
+			Title = "Subscribe to my channel",
+			Subtitle = "Hello",
+			Description = "It's me",
+			BadgeNumber = 42,
+			Schedule = new NotificationRequestSchedule
+			{
+				NotifyTime = DateTime.Now.AddSeconds(1),
+				NotifyRepeatInterval = TimeSpan.FromSeconds(5),
+			}
+		};
+		LocalNotificationCenter.Current.Show(request);
+		
+	}
+}
+
+
